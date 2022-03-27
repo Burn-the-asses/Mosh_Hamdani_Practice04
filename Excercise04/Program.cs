734ef83917015951f82e40f7a3190a09411add00
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-03-27 12:33:13 +0500

    Pascal Exercise in sharp done.

--- Excercise04/Program.cs
+++ Excercise04/Program.cs
@@ -6,7 +6,22 @@ namespace Excercise04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***********************PASCAl Case ******************");
+            System.Console.Write("Enter Few Words: ");
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                System.Console.WriteLine("Error");
+                return;
+            }
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordPascal = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName = variableName + wordPascal;
+            }
+            System.Console.WriteLine(variableName);
         }
     }
 }
